02d643768e7198d8dab9012fe09554b8c8915345
30isnottoolate <akos.varga.92@gmail.com>
2023-02-12 16:01:57 +0100

    Add fs to save scraped data to JSON

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const puppeteer = require('puppeteer');
+const fs = require("fs");
 
 const scrapeData = async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    await page.goto("https://wordfinder.yourdictionary.com/unscramble/asdfjklruei/");
+    await page.goto("https://word.tips/unscramble/asdfjkl/");
 
-    const items = await page.$$eval("td.table__cell--first", items => {
-        return items.map(item => item.textContent);
+    const items = await page.$$eval(".p-1.m-1", items => { //td.table__cell--first
+        console.log(items.length);
+        return items.map(item => item.firstChild.textContent);
     });
 
-    console.log(items);
+    fs.writeFile("scraped-data.json", JSON.stringify(items, null, 4), (error) => {
+        if (error) {
+            console.error(error);
+            return;
+        }
 
+        console.log("Data scraped");
+    });
     browser.close();
 };
 
