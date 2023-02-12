51c3c69415441a9bbdfb7d56babc6772c466bb7e
30isnottoolate <akos.varga.92@gmail.com>
2023-02-12 16:24:08 +0100

    Add try/catch blocks

--- index.js
+++ index.js
@@ -2,24 +2,32 @@ const puppeteer = require('puppeteer');
 const fs = require("fs");
 
 const scrapeData = async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
 
-    await page.goto("https://word.tips/unscramble/asdfjkl/");
+        await page.goto("https://word.tips/unscramble/asdfjkl/");
 
-    const items = await page.$$eval(".p-1.m-1", items => {
-        return items.map(item => item.firstChild.textContent);
-    });
+        const items = await page.$$eval(".p-1.m-1", items => {
+            return items.map(item => item.firstChild.textContent);
+        });
 
-    fs.writeFile("scraped-data.json", JSON.stringify(items, null, 4), (error) => {
-        if (error) {
-            console.error(error);
-            return;
-        }
+        fs.writeFile("scraped-data.json", JSON.stringify(items, null, 4), (error) => {
+            if (error) {
+                console.error(error);
+                return;
+            }
 
-        console.log("Data scraped");
-    });
-    browser.close();
+            console.log("Data scraped");
+        });
+
+        browser.close();
+
+    } 
+    
+    catch (error) {
+        console.error(error);
+    }
 };
 
 scrapeData();
